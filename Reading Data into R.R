d199094e95aebf64df861fca03a12ca7ff84e882
Ahmed Zakaria Ali <ahmedelhatab@gmail.com>
2020-05-05 00:45:29 +0200

    Reading CSV or table in R

---
+++ Reading Data into R.R
@@ -0,0 +1,11 @@
+#import data from CSV file using read.csv or read.csv2
+df<-read.csv("C:\\Users\\Ahmed Zakaria Ali\\
+             Documents\\ADSL Portal Files\\Processed\\
+             Active_ADSL_Users_13022020.csv")
+
+#import data from text file using read.table()
+secondDF<-read.table("C:\\Users\\Ahmed Zakaria Ali\\
+                   Documents\\ADSL Portal Files\\Processed\\
+                   Active_ADSL_Users_13022020.csv",header=True)
+
+j
