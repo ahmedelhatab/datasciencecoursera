ddbc98a2ae224569aea698d63fe1a2c31cc6dcf4
Ahmed Zakaria Ali <ahmedelhatab@gmail.com>
2020-05-11 02:01:43 +0200

    done subsetting

--- subsetting objects in R.R
+++ subsetting objects in R.R
@@ -13,4 +13,21 @@ print(my_list[[1]])
 print(my_list[c(1,2)])
 second_list<-list(a=list(1,2,3,6),c=c(2,4))
 print(second_list[[c(2,1)]])
-
+#subsetting matrixes
+my_matrix<- matrix(data=2:10,nrow=1,ncol=10);
+print(my_matrix)
+#using row and columns indices
+print(my_matrix[1,2])
+#get the complete row.
+print(my_matrix[1,,drop=FALSE])
+print(my_list[["k",exact=FALSE]])
+# Removing null values.
+my_vector<-c(1,NA,3,NA);
+#empty_my_vector<-is.na(my_vector);
+#print(my_vecotr[!empty_my_vector])
+my_character_vector<- c("a",NA,"b",NA)
+good <- complete.cases(my_vector,my_character_vector)
+#complete cases should have the same length for arguments.
+print(my_vector[good])
+ready_values_airquality<-complete.cases(airquality)
+print(airquality[ready_values_airquality,][1:6,])
